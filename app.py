5759de62cb61dc07b5a61822a88c72318b05c3ce
Jon Mease <jon.mease.dev@gmail.com>
2020-12-10 05:45:52 -0500

    Use port 8050 for workspaces compatibility

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ app.layout = dbc.Container([
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8068)
+    app.run_server(debug=True, port=8050)

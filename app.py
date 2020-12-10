d509f1abc61ad4c4ebaef2944fa84465fd1b3ddb
Jon Mease <jon.mease.dev@gmail.com>
2020-12-10 05:47:00 -0500

    Remove PyCharm comment

--- app.py
+++ app.py
@@ -85,6 +85,5 @@ app.layout = dbc.Container([
     components.store,
 ])
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     app.run_server(debug=True, port=8050)
